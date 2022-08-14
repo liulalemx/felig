d6eb055f7a55754ea06357c19f39792bd650a981
liul <48413031+liulaz@users.noreply.github.com>
2022-08-14 22:18:00 +0300

    Added similarity counter

--- src/render.js
+++ src/render.js
@@ -1,5 +1,3 @@
-// import felig_toolkit from "felig-toolkit";
-
 // Selectors
 const fileInput = document.querySelector(".custom-file-input");
 const queryInput = document.querySelector(".searchBar");
@@ -39,9 +37,40 @@ async function logListofFiles() {
     var lastPart = file[0].replace(/\\$/, "").split("\\").pop();
     let li = document.createElement("li");
     li.classList.add("searchResult");
-    li.innerHTML = `<div class="fileDisplay"><h3>${lastPart}</h3><p>${file[0]}</p></div><h4>${file[1]}%</h4>`;
+    li.innerHTML = `<div class="fileDisplay"><h3>${lastPart}</h3><p>${file[0]}</p></div><span class="counter" data-target=${file[1]}>0</span>%`;
     resultList.appendChild(li);
   });
 
+  const counters = document.querySelectorAll(".counter");
+
+  function countUp() {
+    counters.forEach((counter) => {
+      counter.innerText = "0";
+
+      const updateCounter = () => {
+        // Get count target
+        const target = +counter.getAttribute("data-target");
+        // Get current counter value
+        const c = +counter.innerText;
+
+        // Create an increment
+        const increment = target / 100;
+
+        // If counter is less than target, add increment
+        if (c < target) {
+          // Round up and set counter value
+          counter.innerText = `${Math.ceil(c + increment)}`;
+
+          setTimeout(updateCounter, 35);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      updateCounter();
+    });
+  }
+
+  countUp();
   console.log(Object.keys(res));
 }
