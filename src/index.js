48bfc04ec39a82f11d78f63ec43348bf8eec7ef0
liul <48413031+liulaz@users.noreply.github.com>
2022-08-18 13:04:54 +0300

    Added app icon

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "./assets/logo.ico",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
